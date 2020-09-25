7fd8445bb986864cf1a931d9529adb33fb8a09f1
DiogoFdePaula <diogofdepaula@gmail.com>
2020-09-25 17:25:02 -0300

    linha10 OK

--- web/src/pages/atendimento/pages/print/pages/lme/component/linha10/index.js
+++ web/src/pages/atendimento/pages/print/pages/lme/component/linha10/index.js
@@ -1,60 +1,65 @@
 import { Box, Grid, Typography } from '@material-ui/core'
 import React, { useContext } from 'react'
 import { ImpressaoContext } from '../../../..'
+import { format } from 'date-fns';
+import { ptBR } from "date-fns/locale";
 
 const Linha10LME = () => {
 
-    const impressao = useContext(ImpressaoContext)
+    const { impressao } = useContext(ImpressaoContext)
+
+    const date = format(impressao.database, "dd '/' MM '/' yyyy", { locale: ptBR }) 
 
     return (
         <>
             <Box mt={1}>
                 <Grid container spacing={1}>
-                    <Grid container item spacing={1} xs={8} direction="column" justify="center" alignItems="stretch">
-                        
-                        <Grid item>
-                            <Box width={1} border={1} borderColor="black" display="block">
-                                <Box mt={-1} ml={2} display="flex">
-                                    <Typography component={'span'} variant="caption" noWrap={true} >
-                                        <Box bgcolor="white" px={1}>14 - Nome do médico solicitante</Box>
-                                    </Typography>
-                                </Box>
-                                <Box ml={1}>
-                                    <Typography component={'span'} variant={'h6'} align={'left'}>
-                                        <Box>Dr. Diogo F. de Paula</Box>
-                                    </Typography>
-                                </Box>
-                            </Box>
-                        </Grid>
-                        <Grid container item direction="row" alignItems="stretch" spacing={1}>
-                            <Grid item xs={9}>
+                    <Grid container item xs={8}>
+                        <Grid container item direction="column" justify="center" alignItems="stretch" spacing={1}>
+                            <Grid item>
                                 <Box width={1} border={1} borderColor="black" display="block">
                                     <Box mt={-1} ml={2} display="flex">
                                         <Typography component={'span'} variant="caption" noWrap={true} >
-                                            <Box bgcolor="white" px={1}>15 - Número do Cartão Nacional de Saúde (CNS) do médico solicitante</Box>
+                                            <Box bgcolor="white" px={1}>14 - Nome do médico solicitante</Box>
                                         </Typography>
                                     </Box>
                                     <Box ml={1}>
                                         <Typography component={'span'} variant={'h6'} align={'left'}>
-                                            {/* // se tiver que deixar em branco então deixa "-" de cor branca */}
-                                            <Box>980.0162.8089.8038</Box>
+                                            <Box>Dr. Diogo F. de Paula</Box>
                                         </Typography>
                                     </Box>
                                 </Box>
                             </Grid>
-                            <Grid item xs={3}>
-                                <Box height={1} width={1} border={1} borderColor="black" display="block">
-                                    <Box mt={-1} display="flex" justifyContent="center">
-                                        <Typography component={'span'} variant="caption" noWrap={true} >
-                                            <Box bgcolor="white" px={1}>16 - Data da solicitação</Box>
-                                        </Typography>
+                            <Grid container item direction="row" alignItems="stretch" spacing={1}>
+                                <Grid item xs={9}>
+                                    <Box width={1} border={1} borderColor="black" display="block">
+                                        <Box mt={-1} ml={2} display="flex">
+                                            <Typography component={'span'} variant="caption" noWrap={true} >
+                                                <Box bgcolor="white" px={1}>15 - Número do Cartão Nacional de Saúde (CNS) do médico solicitante</Box>
+                                            </Typography>
+                                        </Box>
+                                        <Box ml={1}>
+                                            <Typography component={'span'} variant={'h6'} align={'left'}>
+                                                {/* // se tiver que deixar em branco então deixa "-" de cor branca */}
+                                                <Box>980.0162.8089.8038</Box>
+                                            </Typography>
+                                        </Box>
                                     </Box>
-                                    <Box ml={1}>
-                                        <Typography component={'span'} variant={'h6'} align={'center'}>
-                                            <Box>{impressao.database}</Box>
-                                        </Typography>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Box height={1} width={1} border={1} borderColor="black" display="block">
+                                        <Box mt={-1} display="flex" justifyContent="center">
+                                            <Typography component={'span'} variant="caption" noWrap={true} >
+                                                <Box bgcolor="white" px={1}>16 - Data da solicitação</Box>
+                                            </Typography>
+                                        </Box>
+                                        <Box>
+                                            <Typography component={'span'} variant={'h6'} align={'center'}>
+                                                <Box>{date}</Box>
+                                            </Typography>
+                                        </Box>
                                     </Box>
-                                </Box>
+                                </Grid>
                             </Grid>
                         </Grid>
                     </Grid>
@@ -73,9 +78,6 @@ const Linha10LME = () => {
                         </Box>
                     </Grid>
                 </Grid>
-
-
-
             </Box>
         </>
     )
