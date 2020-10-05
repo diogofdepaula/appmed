6e5e8d0880a9d01ea39a6a59bcfd1009507f1355
Diogo F de Paula <diogofdepaula@gmail.com>
2020-10-05 07:02:14 -0300

    linha11 falta dimencionar

--- web/src/pages/atendimento/pages/print/pages/lme/component/linha1/linha11.js
+++ web/src/pages/atendimento/pages/print/pages/lme/component/linha1/linha11.js
@@ -1,9 +1,9 @@
 import { Box, CardMedia, Grid, Typography } from '@material-ui/core'
 import React, { useContext } from 'react'
 import { ImpressaoContext } from '../../../..'
-import LogoCISCO from '../../../../component/imagens/ciscologo.png'
-import LogoCISGAP from '../../../../component/imagens/cisgaplogo.png'
 import LogoSUS from '../../../../component/imagens/logosuspng.png'
+import LogoMinisterio from '../../../../component/imagens/ministeriologo.png'
+import LogoSESA from '../../../../component/imagens/sesalogo.png'
 
 const Linha11LME = () => {
 
@@ -15,26 +15,32 @@ const Linha11LME = () => {
         <>
             <Box mt={2}>
                 <Grid container direction="row" justify="center" alignItems="stretch">
-                    <Grid item xs style={{backgroundColor: "blue"}}>
-                        <CardMedia
-                            style={{ width: "108px", height: "100%" }}  // width (em px) = heightdaimagemnatela/heightoriginal x widthoriginal
-                            image={LogoSUS}
-                        />
+                    <Grid item xs>
+                        <Box height={1} width={1} display="flex" justifyContent="center">
+                            <CardMedia
+                                style={{ width: "108px", height: "100%" }}  // width (em px) = heightdaimagemnatela/heightoriginal x widthoriginal
+                                image={LogoSUS}
+                            />
+                        </Box>
                     </Grid>
                     <Grid item xs>
-                        {/* <CardMedia
-                                    style={{ width: "135px", height: "100%" }}  // width (em px) = heightdaimagemnatela/heightoriginal x widthoriginal
-                                    image={Ministerio}
-                                    /> */}
-                        <Typography variant={'h3'} >
-                            <Box fontWeight="fontWeightBold" >Ministério da Saúde</Box>
-                        </Typography>
+                        <Box height={1} width={1} display="flex" justifyContent="center">
+                            <CardMedia
+                                style={{ width: "135px", height: "100%" }}  // width (em px) = heightdaimagemnatela/heightoriginal x widthoriginal
+                                image={LogoMinisterio}
+                            />
+                            <Typography variant={'h3'} >
+                                <Box style={{ color: "white" }}>-</Box>
+                            </Typography>
+                        </Box>
                     </Grid>
                     <Grid item xs>
-                        <CardMedia
-                            style={{ width: "135px", height: "100%" }}  // width (em px) = heightdaimagemnatela/heightoriginal x widthoriginal
-                            image={impressao.local === "cisgap" ? LogoCISGAP : LogoCISCO}
-                        />
+                        <Box height={1} width={1} display="flex" justifyContent="center">
+                            <CardMedia
+                                style={{ width: "135px", height: "100%" }}  // width (em px) = heightdaimagemnatela/heightoriginal x widthoriginal
+                                image={LogoSESA}
+                            />
+                        </Box>
                     </Grid>
                 </Grid>
             </Box>
