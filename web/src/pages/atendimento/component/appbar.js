e237f14ba7bd401f92c7aed8d8402fe535286ce3
Diogo F de Paula <diogofdepaula@gmail.com>
2020-11-12 02:43:23 -0300

    para garantir

--- web/src/pages/atendimento/component/appbar.js
+++ web/src/pages/atendimento/component/appbar.js
@@ -65,6 +65,7 @@ const AtendimentoAppBar = () => {
                                     setPrescricaoMain(null),
                                     setPage('prescricoes'),
                                     setAppBarRight('prescricoes')
+                                    //
                                 )
                             }}
                         >
