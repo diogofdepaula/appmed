6931c9f8652129558ffc63da924544bb205f3493
diogofdepaula <diogofdepaula@gmail.com>
2020-12-01 22:32:19 -0300

    garantir

--- web/src/pages/atendimento/pages/lmes/relatorio/components/relatorioset4.js
+++ web/src/pages/atendimento/pages/lmes/relatorio/components/relatorioset4.js
@@ -97,6 +97,8 @@ const RelatorioSet4 = () => {
                                 onChange={handleChangeRadio}
 
                             />
+
+                            
                             <Form.Check
                                 inline
                                 type="radio"
