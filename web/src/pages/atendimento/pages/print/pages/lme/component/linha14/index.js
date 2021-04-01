1a5ed26ec898df0c768ab7a40656f222da0aa512
diogofdepaula <diogofdepaula@gmail.com>
2021-04-01 14:19:23 -0300

    aumetno da fonte do rodap´´e da LME

--- web/src/pages/atendimento/pages/print/pages/lme/component/linha14/index.js
+++ web/src/pages/atendimento/pages/print/pages/lme/component/linha14/index.js
@@ -6,7 +6,7 @@ const Linha14LME = () => {
     return (
         <>
             <Typography component={'span'} variant={'body2'} align={'left'} >
-                <Box mt={1} fontSize={6} display="block">
+                <Box mt={1} fontSize={7} display="block">
                     <Box width={1}>
                         Referência: arquivo disponibilizado no http://www.cmde.parana.pr.gov.br/ . Adequa-se a Portaria 13/2020 quw altera o Título IV do Anexo XXVIII da Portaria de Consolidação nº 2/GM/MS, de 28 de setembro de 2017.
                     </Box>
