c3be4208a626868bda0b952c65e274b57ba23d21
diogofdepaula <diogofdepaula@gmail.com>
2021-04-14 13:06:04 -0300

    const Receita para diferenciar local

--- web/src/pages/atendimento/pages/print/pages/index.js
+++ web/src/pages/atendimento/pages/print/pages/index.js
@@ -10,6 +10,27 @@ const Factory = () => {
 
     const { impressao } = useContext(ImpressaoContext)
 
+    const Receita = ({ lme, via, mes }) => {
+
+        let receita = <div />
+        // variações conforme o local
+        switch (impressao.local) {
+            case 'consultorio':
+                // <FactoryReceitasConsultorio listPresc={lme.prescricoes} via={via} mes={mes} />
+                break;
+            case 'cisgap':
+                receita = <FactoryReceitasSUS listPresc={lme.prescricoes} via={via} mes={mes} />
+                break;
+            case 'cisco':
+                receita = <FactoryReceitasSUS listPresc={lme.prescricoes} via={via} mes={mes} />
+                break;
+
+            default:
+                break;
+        }
+        return receita
+    }
+
     const PrintJob = () => {
 
         let jobs = []
@@ -25,15 +46,15 @@ const Factory = () => {
                     [...Array(6).keys()].map(d =>
                         <div key={d}>
                             {/* tem que passar o valor de cada mes da prescricao para cada receita de cada mês se não sai somente a soma */}
-                            <FactoryReceitasSUS listPresc={l.prescricoes} via={"Estado"} mes={d} />
+                            <Receita lme={l} via={"Estado"} mes={d} />
                         </div>
                     )
                     :
-                    <FactoryReceitasSUS listPresc={l.prescricoes} via={"Estado"} />
+                    <Receita lme={l} via={"Estado"} />
                 }
                 {/* Medicamentos não controlados */}
                 {/* não passar a variável mês, para dar undifined lá nos componentes internos e saber, saber que é via paciente (aí não precisa passar o via paciente) */}
-                <FactoryReceitasSUS listPresc={l.prescricoes} via={"paciente"} />
+                <Receita lme={l} via={"paciente"} />
             </div>
         )
 
@@ -42,9 +63,9 @@ const Factory = () => {
         }
 
         //print prescricoesSelecionadas
-        if (impressao.prescricoesSelecionadas.length > 0) {
-            jobs.push(<div key={0}><FactoryReceitasSUS listPresc={impressao.prescricoesSelecionadas} /></div>)
-        }
+        // if (impressao.prescricoesSelecionadas.length > 0) {
+        //     jobs.push(<div key={0}><FactoryReceitasSUS listPresc={impressao.prescricoesSelecionadas} /></div>)
+        // }
 
         return jobs
 
