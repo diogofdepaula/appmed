376d7e4de55be70f57b17ce42d7a8587c3a4f8aa
diogofdepaula <diogofdepaula@gmail.com>
2021-05-03 10:50:44 -0300

    apagando Box do pageA5

--- web/src/pages/atendimento/pages/print/pages/receita/consultorio/index.js
+++ web/src/pages/atendimento/pages/print/pages/receita/consultorio/index.js
@@ -1,28 +1,22 @@
 import { Box, Typography } from '@material-ui/core';
-import React, { useContext } from 'react';
-import { ImpressaoContext } from '../../../../..';
+import React from 'react';
 import PageA5 from '../../component/pagea5';
 
 const ReceitaConsultorio = (props) => {
 
-    const { impressao } = useContext(ImpressaoContext)
-
     return (
         <>
             <PageA5>
-                <Box height={1} width={1} pt={impressao.pagesize.a5.pt} pl={impressao.pagesize.a5.pl} pb={impressao.pagesize.a5.pb} pr={impressao.pagesize.a5.pr} style={{ backgroundColor: 'yellow' }}>
-                    <Typography component={'span'} variant={'h6'} align={'justify'}>
-                        <Box height={1} width={1} style={{ backgroundColor: 'white' }}>
-                            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                            quae ab illo inventore veritatis et quasi architecto beatae
-                            vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
-                            voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
-                            magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+                <Typography component={'span'} variant={'h6'} align={'justify'}>
+                    <Box height={1} width={1} style={{ backgroundColor: 'white' }}>
+                        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                        quae ab illo inventore veritatis et quasi architecto beatae
+                        vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                        voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+                        magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
                         </Box>
-                    </Typography>
-
-                </Box>
+                </Typography>
             </PageA5>
         </>
     )
