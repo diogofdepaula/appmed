d0f874d99dd1593480f62c51c46fe53c915d3163
diogofdepaula <diogofdepaula@gmail.com>
2021-05-11 18:55:34 -0300

    identificação com useStyle

--- web/src/pages/atendimento/pages/print/pages/receita/component/identificacao.js
+++ web/src/pages/atendimento/pages/print/pages/receita/component/identificacao.js
@@ -1,24 +1,46 @@
 import { Box, Typography } from '@material-ui/core'
 import React, { useContext } from 'react'
 import { ClienteContext } from '../../../../../../../App'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    // root: {
+    //     display: 'flex',
+    //     backgroundColor: theme.palette.common.white
+    // },
+    box: {
+        display: 'block',
+        paddingTop: 3,
+        paddingBottom: 7,
+    },
+    typonome: {
+        fontSize: 42,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    typocpf: {
+        fontVariant: 'subtitle2',
+        textAlign: 'center'
+    }
+}));
+
 
 const Identificacao = () => {
+
+    const classes = useStyles();
 
     const { clienteContext } = useContext(ClienteContext)
 
     return (
         <>
-            <Box
-                display="block"
-                pt={3}
-                pb={7}
-            >
-                <Typography variant={'h2'} align="center" >
-                    <Box fontWeight="fontWeightBold" >{clienteContext.nome}</Box>
+            <Box className={classes.box} >
+                <Typography className={classes.typonome} >
+
+                    {clienteContext.nome}
                 </Typography>
                 {clienteContext.cpf &&
-                    <Typography variant={'subtitle2'} align="center" >
-                        <Box>CPF: {clienteContext.cpf} </Box>
+                    <Typography className={classes.typocpf}  >
+                        CPF: {clienteContext.cpf}
                     </Typography>
                 }
             </Box>
